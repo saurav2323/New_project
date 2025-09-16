57f30dbb6ce8008f6e682cb3836f3d994947f7a0
sauravhub <kumarsaurav23230@gmail.com>
2025-09-16 20:39:08 +0530

    add Hello class with main method and addition functionality

---
+++ Java/Hello.java
@@ -0,0 +1,10 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
